341b49442e50bf0e8edfbb9fde736c1eb923c294
Jonathan Rein <jrein1296@gmail.com>
2021-03-22 19:48:10 -0400

    Temp now displays on page!

--- assets/script.js
+++ assets/script.js
@@ -4,9 +4,12 @@ var humidity;
 var windSpeed
 var UvIndex
 var weatherIcon;
+var showCurrentTemp = document.getElementById("showCurrentTemp");
+
+
 
 function getWeather() {
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=atlanta&appid=4b37cdd7653dfc3582c009509a56e3eb';
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=atlanta&units=imperial&appid=4b37cdd7653dfc3582c009509a56e3eb';
 
     fetch(requestUrl) 
     .then(function (response) {
@@ -14,7 +17,11 @@ function getWeather() {
     })
     .then(function (data) {
       currentTemp = data.main.temp
-      console.log(currentTemp)
+      console.log(currentTemp),
+      showCurrentTemp.textContent = "Temperature: " + currentTemp + " °F" ;
+      
     })
     
   }; getWeather()
+
+  

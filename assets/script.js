7b01f90e6f2bae893ab11ca518b19a87d471b7fa
Jonathan Rein <jrein1296@gmail.com>
2021-03-22 17:03:01 -0400

    Updating progress of getWeather function, not working.

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,21 @@
 var location;
-var temp;
+ 
 var humidity;
 var windSpeed
 var UvIndex
 var weatherIcon;
 
 function getWeather() {
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={4b37cdd7653dfc3582c009509a56e3eb}';
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=atlanta&appid=4b37cdd7653dfc3582c009509a56e3eb';
 
-    fetch(requestUrl)
+    fetch(requestUrl) 
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       console.log(data);
-    })}; getWeather()
+      console.log(data.main.temp)
+      var currentTemp = data.main.temp
+    })
+    
+  }; getWeather()

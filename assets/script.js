017f41077b47368bf6061c416f5926c892c2152a
Jonathan Rein <jrein1296@gmail.com>
2021-03-22 21:34:04 -0400

    Search input saves to local storage.

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,34 @@
 var location;
-var currentTemp
-var humidity;
-var windSpeed
-var UvIndex
-var weatherIcon;
-var showCurrentTemp = document.getElementById("showCurrentTemp");
+var currentTemp;
+var currentHumid;
+var currentWind;
+var currentUv;
+var currentIcon;
 
+var showCityDate = document.getElementById("showCityDate");
+var showCurrentTemp = document.getElementById("showCurrentTemp");
+var showCurrentHumid = document.getElementById("showCurrentHumid");
+var showCurrentWind = document.getElementById("showCurrentWind");
+// var showCurrentUv = document.getElementById("showCurrentUv");
+var showCurrentIcon = document.getElementById("showCurrentIcon")
+var citySearch = document.getElementById("citySearch");
+var searchBtn = $("#searchBtn");
+
+
+// displays current date with clock
+setInterval(function(){
+  var date = moment().format('MMMM Do YYYY, h:mm a');
+  showCityDate.textContent = "Current City " + date;
+});
+
+
+// gets user input from search for city field
+searchBtn.on('click', function () {
+  console.log("I was clicked")
+  var selectedCity = $("#citySearch").val();
+
+  localStorage.setItem("citySearch", JSON.stringify(selectedCity));
+});
 
 
 function getWeather() {
@@ -16,9 +39,23 @@ function getWeather() {
       return response.json();
     })
     .then(function (data) {
-      currentTemp = data.main.temp
-      console.log(currentTemp),
-      showCurrentTemp.textContent = "Temperature: " + currentTemp + " °F" ;
+      console.log(data)
+      currentTemp = data.main.temp.toFixed(1);
+      currentHumid = data.main.humidity;
+      currentWind = data.wind.speed,
+      // currentUv =
+      currentIcon = data.weather[0].icon;
+      
+      
+      showCurrentTemp.textContent = "Temperature: " + currentTemp + "°F" ;
+      showCurrentHumid.textContent = "humidity: " + currentHumid + "%";
+      showCurrentWind.textContent = "Wind Speed: " + currentWind + "mph";
+      // showCurrentUv.textContent = "UV Index: "
+      // showCurrentIcon.textContent = currentIcon;
+
+      
+
+      
       
     })
     

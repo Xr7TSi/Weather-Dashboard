193c81fd40e260070a32c6e1885d77686d70e607
Jonathan Rein <jrein1296@gmail.com>
2021-03-24 21:07:24 -0400

    Adding script.temp.js.  Will be used to combine weather funcitions into one function that takes in a city name for a varaibale.

---
+++ assets/script.temp.js
@@ -0,0 +1,73 @@
+
+
+
+function getWeather(selectedCity) {
+
+
+
+ var weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?&units=imperial&appid=4b37cdd7653dfc3582c009509a56e3eb&q=' + selectedCity;
+
+ fetch(weatherUrl) 
+ .then(function (response) {
+   return response.json();
+ })
+ .then(function (data) {
+   currentTemp = data.main.temp.toFixed(0);
+   currentHumid = data.main.humidity;
+   currentWind = data.wind.speed.toFixed(0);
+     
+   showCurrentTemp.textContent = "Temperature: " + currentTemp + "°F" ;
+   showCurrentHumid.textContent = "humidity: " + currentHumid + "%";
+   showCurrentWind.textContent = "Wind Speed: " + currentWind + "mph";
+ })
+
+
+ // api for forecast
+ var forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?&units=imperial&appid=279105e2e4e9e82f777d589c68abec56&q=' + selectedCity;
+
+ fetch(forecastUrl) 
+ .then(function (response) {
+   return response.json();
+ })
+ .then(function (data) {
+   dayOneH = data.list[0].main.temp_max.toFixed(0);
+   dayOneL = data.list[0].main.temp_min.toFixed(0);
+   dayOneHumid = data.list[0].main.humidity;
+   dayTwoH = data.list[1].main.temp_max.toFixed(0);
+   dayTwoL = data.list[1].main.temp_min.toFixed(0);
+   dayTwoHumid = data.list[1].main.humidity;
+   
+   dayThreeH = data.list[2].main.temp_max.toFixed(0);
+   dayThreeL = data.list[2].main.temp_min.toFixed(0);
+   dayThreeHumid = data.list[2].main.humidity;
+
+   dayFourH = data.list[3].main.temp_max.toFixed(0);
+   dayFourL = data.list[3].main.temp_min.toFixed(0);
+   dayFourHumid = data.list[3].main.humidity;
+
+   dayFiveH = data.list[4].main.temp_max.toFixed(0);
+   dayFiveL = data.list[4].main.temp_min.toFixed(0);
+   dayFiveHumid = data.list[4].main.humidity;
+
+   showDayOneH.textContent = "High: " + dayOneH + "°F";
+   showDayOneL.textContent = "Low: " + dayOneL + "%";
+   showDayOneHumid.textContent = "Humidity: " + dayOneHumid + "%";
+
+   showDayTwoH.textContent = "High: " + dayTwoH + "°F";
+   showDayTwoL.textContent = "Low: " + dayTwoL + "%";
+   showDayTwoHumid.textContent = "Humidity: " + dayTwoHumid + "%";
+
+   showDayThreeH.textContent = "High: " + dayThreeH + "°F";
+   showDayThreeL.textContent = "Low: " + dayThreeL + "%";
+   showDayThreeHumid.textContent = "Humidity: " + dayThreeHumid + "%";
+
+   showDayFourH.textContent = "High: " + dayFourH + "°F";
+   showDayFourL.textContent = "Low: " + dayFourL + "%";
+   showDayFourHumid.textContent = "Humidity: " + dayFourHumid + "%";
+
+   showDayFiveH.textContent = "High: " + dayFiveH + "°F";
+   showDayFiveL.textContent = "Low: " + dayFiveL + "%";
+   showDayFiveHumid.textContent = "Humidity: " + dayFiveHumid + "%";
+   })
+
+}
